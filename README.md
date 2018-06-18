aff78ef132202be03879ac3625edd4ffd2195128
Mark <markteams@gmail.com>
2018-06-18 21:55:20 +0300

    evaluation model added, some bugfix and enhancements.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # recommendation-engine
-Using Apriori algorithm for affinity analysis on movie dataset and extracting association rules as movie recommendation
+Implementing Apriori algorithm for affinity analysis on movie dataset and extracting association rules as movie recommendation.
+
+Affinity analysis is the task of determining when objects are used in similar ways.
+Affinity analysis is usually much more exploratory than classification. We often don't have the complete dataset we expect for many classification tasks.
+
+The classic algorithm for affinity analysis is called the Apriori algorithm. It addresses the exponential problem of creating sets of items that occur frequently within a database, called frequent itemsets. Once these frequent itemsets are discovered, creating association rules is straightforward.
+
+The intuition behind Apriori is both simple and clever. First, we ensure that a rule has sufficient support within the dataset. Defining a minimum support level is the key parameter for Apriori. To build a frequent itemset, for an itemset (A, B) to have a support of at least 30, both A and B must occur at least 30 times in the database. This property extends to larger sets as well. For an itemset (A, B, C, D) to be considered frequent, the set (A, B, C) must also be frequent (as must D).
+
+These frequent itemsets can be built up and possible itemsets that are not frequent (of which there are many) will never be tested. This saves significant time in testing new rules.
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-## import libraries -----------------------------
-import numpy as np
+# import libraries
 import pandas as pd
 import os
 import sys
@@ -8,13 +7,18 @@ from collections import defaultdict
 
 
 class MovieRecommendation:
+    user_threshold: int
+    min_support: int
+    min_confidence: float
 
     def __init__(self):
+        # users in train set
+        self.user_threshold = 200
         # minimum support for movies 
         self.min_support = 50
         # Defining a minimum for confidence level
         self.min_confidence = 0.9
-        self.frequent_itemsets = {}
+        self.itemsets = {}
         # Dataset placeholders
         self.ratings_full = {}
         self.ratings = {}  # Minimise version of the whole dataset
@@ -29,7 +33,7 @@ class MovieRecommendation:
         movie_name_filename = os.path.join(data_folder, "movies.dat")
 
         # Defining dateparser for reading date column
-        dateparse = lambda x: pd.to_datetime(x, unit='s')
+        date_parser = lambda x: pd.to_datetime(x, unit='s')
 
         # Reading the reviews file and defining the columns name
         self.ratings_full = pd.read_csv(rating_filename,
@@ -37,7 +41,7 @@ class MovieRecommendation:
                                         header=None,
                                         names=['UserID', 'MovieID', 'Rating', 'Datetime'],
                                         parse_dates=['Datetime'],
-                                        date_parser=dateparse,
+                                        date_parser=date_parser,
                                         engine='python')
 
         # Reading movies dat file and setting column names
@@ -64,69 +68,90 @@ class MovieRecommendation:
                                  for user_id, movies in \
                                  favorable_ratings.groupby("UserID")["MovieID"])
 
-    def create_initial_freq_itemsets(self):
+    def create_initial_itemset(self):
         # Frequency of each movie given a favorable review
         movie_freq = self.ratings[['MovieID', 'Favorable']].groupby('MovieID').sum()
 
-        # superset_length=1 candidates are the isbns with more than min_support favourable reviews
-        self.frequent_itemsets[1] = dict((frozenset((movie_id,)), row["Favorable"]) \
-                                         for movie_id, row in movie_freq.iterrows() \
-                                         if row["Favorable"] > self.min_support)
+        """
+        The structure of itemsets will be as a dictionary with following format:
+        
+        Structure:
+            {length_of_itemset: {(set_of_movies_list_in_current_itemset): frequency_of_itemset},}
+        Key: int
+            length of itemset
+        Value: dict
+            Key: frozenset  
+                a frozenset of list of involving movies in current itemset
+            Value: int
+                how many times current combination of movies occurred in user ratings
+            
+        Example:
+            for a itemset comprises of 3 movies
+            {(movie_1,movie_15,movie_495) : 59}
+        """
 
-        print("{} frequent itemsets in length of {}".format(len(self.frequent_itemsets[1]), 1))
+        # itemset_length=1 are a list of all movies which have rating more than min_support
+        self.itemsets[1] = dict((frozenset((movie_id,)), row["Favorable"])
+                                for movie_id, row in movie_freq.iterrows()
+                                if row["Favorable"] > self.min_support)
+
+        print("[length:itemsets]: ({}:{})".format(1, len(self.itemsets[1])))
         sys.stdout.flush()
 
-    def create_supersets(self, superset_max_size=15):
+    def create_freq_itemsets(self, superset_max_size=15):
+        print("Itemsets creation is in progress, be patient...\n")
+        sys.stdout.flush()
+
         # Creating the first itemsets 
-        self.create_initial_freq_itemsets()
+        self.create_initial_itemset()
 
         # Creating further itemsets with size bigger than 2
-        for superset_length in range(2, superset_max_size):
-            # Generate candidates of length superset_length, using the frequent itemsets of its precedure
-            candidate_superset_freq = defaultdict(int)
+        for superset_length in range(2, superset_max_size + 1):
 
+            # Finding candidate itemsets in various lengths up to super_max_size based on preceding itemset
+            candidate_superset_freq = defaultdict(int)
             for user_id, user_movies in self.users_movies.items():
-                # Check if each itemset in the last freq_itemsets exist in user movies 
-                for itemset in self.frequent_itemsets[superset_length - 1]:
+                for itemset in self.itemsets[superset_length - 1]:
+                    # Check if itemset is a subset of user favorite movies
                     if itemset.issubset(user_movies):
-                        # Construct next level superset based other favoriate movies of users exclude the current
-                        # itemset itself
-                        for other_reviewed_movie in user_movies - itemset:
-                            current_superset = itemset | frozenset((other_reviewed_movie,))
-                            # increase the frequency of recent superset which just occured
+                        # Construct superset with union of current itemset and each of another movies
+                        # which user liked separately
+                        for other_reviewed_movie in user_movies - itemset: # exclude current movies in itemset first
+                            current_superset = itemset | frozenset((other_reviewed_movie,)) # union each remaining itemset
+                            # increase the frequency of recent superset which just occurred
                             candidate_superset_freq[current_superset] += 1
 
-            new_frequent_itemsets = dict([(itemset, frequency)
-                                          for itemset, frequency in candidate_superset_freq.items()
-                                          if frequency >= self.min_support])
+            # Checking for frequency of any recent built itemset (candidates) again minimum threshold
+            superset = dict([(candidate_superset, candidate_superset_frequency)
+                            for candidate_superset, candidate_superset_frequency in candidate_superset_freq.items()
+                            if candidate_superset_frequency >= self.min_support])
 
-            if len(new_frequent_itemsets):
-                print("{} frequent itemsets in length of {}".format(len(new_frequent_itemsets), superset_length))
-                sys.stdout.flush()
-                self.frequent_itemsets[superset_length] = new_frequent_itemsets
+            print("[length:itemsets]: ({}:{})".format(superset_length, len(superset)))
+            sys.stdout.flush()
 
-            elif len(new_frequent_itemsets) == 0:
-                print("There is no frequent itemsets in length of {}".format(superset_length))
+            if len(superset):
+                self.itemsets[superset_length] = superset
+            elif len(superset) == 0:
+                print("No further exploring.")
                 sys.stdout.flush()
                 break
 
         # Itemsets in length 1 are not useful for recommending system so we can drop it
-        del self.frequent_itemsets[1]
+        del self.itemsets[1]
 
-        print("Total numer of itemsets in any length: {0}".format(
-            sum(len(itemsets) for itemsets in self.frequent_itemsets.values())))
+        print('\nItemsets total count: {0}'.format(sum(len(itemsets) for itemsets in self.itemsets.values())))
 
     def extract_association_rules(self):
         """
-        Our approach for identifying rules are simle,
-        iterating over freq_itemsets and within each itemset
-        excluding each member of set and considering it as conclusion
-        and all remaining as premises.
+        In order to identifying association rules we have to iterate over all itemsets and within each itemset
+        pick each member and consider it as conclusion and all others as premises at a time.
         """
         candidate_rules = []
-        for itemset_length, itemset_counts in self.frequent_itemsets.items():
-            for itemset in itemset_counts.keys():
+        for itemset_length, itemset_dict in self.itemsets.items():
+            for itemset in itemset_dict.keys():
+                # selecting each item in itemset and consider it as conclusion
                 for conclusion in itemset:
+                    # making premise set by excluding conclusion
                     premise = itemset - set((conclusion,))
                     candidate_rules.append((premise, conclusion))
 
@@ -146,33 +171,90 @@ class MovieRecommendation:
                     else:
                         invalid_rule[candidate_rule] += 1
 
-        # Calculating confidence rule for all the extracted rules                
-        rule_confidence = {
-            candidate_rule: valid_rule[candidate_rule] / float(
-                valid_rule[candidate_rule] + invalid_rule[candidate_rule])
+        # Calculating confidence level for candidate_rules
+        rules_confidence = {
+            candidate_rule: valid_rule[candidate_rule] / float(valid_rule[candidate_rule] +
+                                                               invalid_rule[candidate_rule])
             for candidate_rule in candidate_rules}
 
         # Filter out the rules with poor confidence
-        self.significant_rules = {rule: confidence for rule, confidence in rule_confidence.items() if
-                                  confidence > self.min_confidence}
+        self.significant_rules = {rule: confidence for rule, confidence in rules_confidence.items()
+                                  if confidence > self.min_confidence}
 
-        print("In total {} rules found which {} of them are significant.".format(len(candidate_rules),
-                                                                                 len(self.significant_rules)))
+        print("Among {} candidate rules only which {} of them are significant.".format(len(candidate_rules),
+                                                                                       len(self.significant_rules)))
 
     def get_movie_name(self, movie_id):
         return self.movies.loc[self.movies["MovieID"] == movie_id, 'Title'].values[0]
 
-    def report_associations(self):
+    def report_associations(self, rule_count=10):
+        # Sorting significant rules dictionary based on significant level
         sorted_confidence = sorted(self.significant_rules.items(), key=itemgetter(1), reverse=True)
-        for index in range(5):
-            print("Rule #{0}".format(index + 1))
+
+        for index in range(rule_count):
+
             (premise, conclusion) = sorted_confidence[index][0]
-            premise_names = ", ".join(self.get_movie_name(movie_id=mov_id) for mov_id in premise)
+            premise_names = "\n ".join(self.get_movie_name(movie_id=mov_id) for mov_id in premise)
             conclusion_name = self.get_movie_name(movie_id=conclusion)
 
-            print(
-                "Rule: If a person recommends {0} they will also recommend {1}".format(premise_names, conclusion_name))
-            print(" - Confidence: {0:.3f}".format(self.significant_rules[(premise, conclusion)]))
+            print("Rule rank #{0} (confidence {1:.3f}):".format(index + 1,
+                                                                self.significant_rules[(premise, conclusion)]))
+
+            print("If a person recommends:\n {0} \nThey will also recommend: \n {1}".format(premise_names,
+                                                                                            conclusion_name))
+            print("\n")
+
+    def evaluate_model(self, rule_count=10):
+        # Make a test dataset to evaluate model
+        test_df = self.ratings_full[self.ratings_full.UserID > self.user_threshold]
+        test_fav = test_df[test_df["Favorable"]]
+
+        test_users_movies = dict((test_user_id, frozenset(movies))
+                                 for test_user_id, movies in
+                                 test_fav.groupby("UserID")["MovieID"])
+
+        candidate_rules = []
+        for itemset_length, itemset_dict in self.itemsets.items():
+            for itemset in itemset_dict.keys():
+                for conclusion in itemset:
+                    premise = itemset - set((conclusion,))
+                    candidate_rules.append((premise, conclusion))
+
+        # Same evaluation as what we have done in extarcting association rules
+        valid_rule = defaultdict(int)
+        invalid_rule = defaultdict(int)
+
+        for user, fav_movies in test_users_movies.items():
+            for candidate_rule in candidate_rules:
+                premise, conclusion = candidate_rule
+                # If user liked all premise movies
+                if premise.issubset(fav_movies):
+                    # If user liked conclusion movie too
+                    if conclusion in fav_movies:
+                        # Then rule should be considered as valid
+                        valid_rule[candidate_rule] += 1
+                    else:
+                        invalid_rule[candidate_rule] += 1
+
+        test_confidence = {candidate_rule: valid_rule[candidate_rule] / float(
+            valid_rule[candidate_rule] + invalid_rule[candidate_rule])
+                           for candidate_rule in candidate_rules}
+
+        sorted_confidence = sorted(self.significant_rules.items(), key=itemgetter(1), reverse=True)
+
+        for index in range(rule_count):
+            (premise, conclusion) = sorted_confidence[index][0]
+            premise_names = "\n ".join(self.get_movie_name(movie_id=mov_id) for mov_id in premise)
+            conclusion_name = self.get_movie_name(movie_id=conclusion)
+
+            print("Rule rank #{0} \n({1:.3f} confidence )\n({2:.3f} test confidence):".format(index + 1,
+                                                                                         self.significant_rules[
+                                                                                             (premise, conclusion)],
+                                                                                         test_confidence.get(
+                                                                                             (premise, conclusion),
+                                                                                             -1)))
+            print("If a person recommends:\n {0} \nThey will also recommend: \n {1}".format(premise_names,
+                                                                                          conclusion_name))
             print("\n")
 
 
@@ -185,10 +267,13 @@ if __name__ == '__main__':
     engine.data_prep(user_threshold=200)
 
     # Constructing itemsets
-    engine.create_supersets(superset_max_size=5)
+    engine.create_freq_itemsets(superset_max_size=15)
 
     # Making association rules
     engine.extract_association_rules()
 
     # Printing reports of extracted rules
     engine.report_associations()
+
+    # Evaluate model
+    engine.evaluate_model()
